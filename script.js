e37e73417471bde060dcc4c48116908a2299dc62
ShriGanesh Saini <67692684+rvsaini8107@users.noreply.github.com>
2023-04-30 11:00:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots.sort();
+
+
 const bandsId = document.getElementById("bands");
-let li = createElement("li");
-li.innerText="hello";
-bandsId.appendChild(li);
+for(let i=0;i<touristSpots.length;i++){
+	let li = document.createElement("li");
+	li.innerText=touristSpots[i];
+	bandsId.appendChild(li);
+	
+}
